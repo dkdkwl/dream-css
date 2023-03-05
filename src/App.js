042167a7757dc2250baacd10d6027b1eafb78d00
dkdkwl <dkdkwl@naver.com>
2023-03-05 17:27:40 +0900

    css 라이브러리 사용하기

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@
 import './App.css';
 import Button1 from './component/Button1';
 import Button2 from './component/Button2';
+import StyleComponent from './StyleComponent';
+import TailwindComponent from './TailwindComponent';
+
 
 function App() {
   return (
     <>
       <Button1></Button1>
       <Button2></Button2>
+    <StyleComponent></StyleComponent>
+    <TailwindComponent></TailwindComponent>
     </>
-  );
+  )
 }
 
 export default App;
